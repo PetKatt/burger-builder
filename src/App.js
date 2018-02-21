d5b3f62dcb4e3f6dcc5ec6828a6c34e8b1091667
Piotr Wiercinski <piotrwier@o2.pl>
2018-02-21 09:17:42 +0100

    Adding inline-style dynamically DONE

--- src/App.js
+++ src/App.js
@@ -54,7 +54,8 @@ class App extends Component {
 
   render() {
   	const style = {
-  		backgroundColor: "white",
+  		backgroundColor: "green",
+      color: "white",
   		font: "inherit",
   		border: "2px solid blue",
   		padding: "8px",
@@ -79,6 +80,8 @@ class App extends Component {
           }
         </div>
       );
+
+      style.backgroundColor = "red";
     }
 
     return (
