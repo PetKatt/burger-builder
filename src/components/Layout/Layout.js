5a0d0608f85f6615af46dd838ad34bfe0b249ace
Piotr Wiercinski <piotrwier@o2.pl>
2018-03-31 11:56:47 +0200

    basic branch: BurgerBuilder container created

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,12 @@
 import React from "react";
 
+import classes from "./Layout.css";
+
 // type of Higher Order Component
 const layout = (props) => (
 	<React.Fragment>
 		<div>Toolbar, SideDrawer, Backdrop</div>
-		<main>
+		<main className={classes.Content}>
 			{props.children}
 		</main>
 	</React.Fragment>
