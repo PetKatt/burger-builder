f511a19c5e69116348e26b28e817d2c44a7fb764
Piotr Wiercinski <piotrwier@o2.pl>
2018-04-02 11:17:40 +0200

    basic branch: removing ingredients DONE

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -39,16 +39,42 @@ class BurgerBuilder extends Component {
 		}));
 	}
 
-	removeIngredient = (type) => {
+	removeIngredientHandler = (type) => {
+		const oldCount = this.state.ingredients[type];
+		if (oldCount <= 0) {
+			return;
+		}
+		const updatedCount = oldCount - 1;
+		const updatedIngredients = {
+			...this.state.ingredients
+		};
+		updatedIngredients[type] = updatedCount;
 
+		const priceDeduction = INGREDIENT_PRICES[type];
+		const oldPrice = this.state.totalPrice;
+		const newPrice = oldPrice - priceDeduction;
+
+		this.setState(() => ({
+			ingredients: updatedIngredients,
+			totalPrice: newPrice
+		}));
 	}
 
 	render() {
+		const disabledInfo = {
+			...this.state.ingredients
+		};
+		for (let key in disabledInfo) {
+			disabledInfo[key] = ( disabledInfo[key] <= 0 );
+		}
+
 		return (
 			<React.Fragment>
 				<Burger ingredients={this.state.ingredients} />
 				<BuildControls
-					ingredientAdded={this.addIngredientHandler} />
+					ingredientAdded={this.addIngredientHandler}
+					ingredientRemoved={this.removeIngredientHandler}
+					disabled={disabledInfo} />
 			</React.Fragment>
 		);
 	}
