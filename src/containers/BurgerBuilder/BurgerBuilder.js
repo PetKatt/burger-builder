f9881a6654228d63fd09a39619d5817caa2f72f9
Piotr Wiercinski <piotrwier@o2.pl>
2018-04-07 13:17:35 +0200

    basic branch: Modal and OrderSummary components Added

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,6 +2,8 @@ import React, { PureComponent } from "react";
 
 import Burger from "../../components/Burger/Burger";
 import BuildControls from "../../components/BuildControls/BuildControls";
+import Modal from "../../components/UI/Modal/Modal";
+import OrderSummary from "../../components/Burger/OrderSummary/OrderSummary";
 
 const INGREDIENT_PRICES = {
 	salad: 0.5,
@@ -89,6 +91,9 @@ class BurgerBuilder extends PureComponent {
 
 		return (
 			<React.Fragment>
+				<Modal>
+					<OrderSummary ingredients={this.state.ingredients} />
+				</Modal>
 				<Burger ingredients={this.state.ingredients} />
 				<BuildControls
 					ingredientAdded={this.addIngredientHandler}
