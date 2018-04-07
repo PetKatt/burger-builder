36ad2d017639947f758c648807aa17c7f0490ef7
Piotr Wiercinski <piotrwier@o2.pl>
2018-04-07 17:06:31 +0200

    basic branch: Toolbar Added

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,12 @@
 import React from "react";
 
 import classes from "./Layout.css";
+import Toolbar from "../Navigation/Toolbar/Toolbar";
 
 // type of Higher Order Component
 const layout = (props) => (
 	<React.Fragment>
-		<div>Toolbar, SideDrawer, Backdrop</div>
+		<Toolbar />
 		<main className={classes.Content}>
 			{props.children}
 		</main>
