6eeffff4fe88ba4ac49d85aa973444db12e7f648
Piotr Wiercinski <piotrwier@o2.pl>
2018-04-07 12:10:18 +0200

    basic branch: checkout button added [updatePurchaseState()]

--- src/components/BuildControls/BuildControls.js
+++ src/components/BuildControls/BuildControls.js
@@ -20,7 +20,10 @@ const buildControls = (props) => (
 				added={() => props.ingredientAdded(ctrl.type)}
 				removed={() => props.ingredientRemoved(ctrl.type)}
 				disabled={props.disabled[ctrl.type]} />			
-		))}		
+		))}
+		<button 
+			className={classes.OrderButton}
+			disabled={!props.purchasable}>ORDER NOW</button>		
 	</div>
 );
 
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { PureComponent } from "react";
 
 import Burger from "../../components/Burger/Burger";
 import BuildControls from "../../components/BuildControls/BuildControls";
@@ -10,7 +10,7 @@ const INGREDIENT_PRICES = {
 	bacon: 0.7
 };
 
-class BurgerBuilder extends Component {
+class BurgerBuilder extends PureComponent {
 	state = {
 		ingredients: {
 			salad: 0,
@@ -18,7 +18,22 @@ class BurgerBuilder extends Component {
 			cheese: 0,
 			meat: 0
 		},
-		totalPrice: 4
+		totalPrice: 4,
+		purchasable: false
+	}
+
+	updatePurchaseState(ingredients) {
+		const sum = Object.keys(ingredients)
+			.map((igKey) => {
+				return ingredients[igKey];
+			})
+			.reduce((sum, el) => {
+				return sum + el;
+			}, 0);
+
+		this.setState(() => ({
+			purchasable: sum > 0
+		}));
 	}
 
 	addIngredientHandler = (type) => {
@@ -37,6 +52,8 @@ class BurgerBuilder extends Component {
 			ingredients: updatedIngredients,
 			totalPrice: newPrice
 		}));
+
+		this.updatePurchaseState(updatedIngredients);
 	}
 
 	removeIngredientHandler = (type) => {
@@ -58,6 +75,8 @@ class BurgerBuilder extends Component {
 			ingredients: updatedIngredients,
 			totalPrice: newPrice
 		}));
+
+		this.updatePurchaseState(updatedIngredients);
 	}
 
 	render() {
@@ -75,6 +94,7 @@ class BurgerBuilder extends Component {
 					ingredientAdded={this.addIngredientHandler}
 					ingredientRemoved={this.removeIngredientHandler}
 					disabled={disabledInfo}
+					purchasable={this.state.purchasable}
 					price={this.state.totalPrice} />
 			</React.Fragment>
 		);
